2e7e5ae846d67cbbe8775a7ca66ad197ebcfc200
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-22 06:27:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,11 +8,11 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    new_price = 100 + price * quantity
+    self.total = self.total + price * quantity
   end
   
   def apply_discount
-    
+    new_price = 100 - (100 * @discount / 100)
   end 
   
   
