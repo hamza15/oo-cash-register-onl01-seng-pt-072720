f10e60c7af1611839891efd9cf537559fe9b388f
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-22 05:48:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=0)
-    self.total.Float = self.total + price + quantity
+    self.total.Float = self.total.Float + price + quantity
   end
   
   
